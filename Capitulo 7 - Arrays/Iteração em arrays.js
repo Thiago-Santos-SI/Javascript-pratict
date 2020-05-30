66428a6f8758d21563bf90ea18447a42da4ad17e
Thiago-Santos-SI <thiiagosantos300@gmail.com>
2020-05-30 14:54:14 -0300

    practicing

---
+++ Capitulo 7 - Arrays/Iteração em arrays.js
@@ -0,0 +1,8 @@
+
+
+const keys = Object.keys(0);
+let values = [];
+for (let i = 0; i < keys.length; i++){
+    let key = keys[i];
+    values[i] = 0[keys];
+}
---
+++ Capitulo 7 - Arrays/arrays.js
@@ -0,0 +1,15 @@
+const empty = [];
+
+const base = 1014;
+const table = [base, base+1, base+2]
+const b = [[1,{x:1, y:2}], [2, {x: 3, y: 4}]]
+
+const a = new Array(10)
+
+empty.push('oi')
+
+console.log(empty)
+
+delete empty[0]
+
+console.log(empty)
