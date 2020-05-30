eca756c05aaec5d2a756ab7f9be4686e97a909a6
Thiago-Santos-SI <thiiagosantos300@gmail.com>
2020-05-30 15:06:16 -0300

    practicing

--- Capitulo 7 - Arrays/Iteração em arrays.js
+++ Capitulo 7 - Arrays/Iteração em arrays.js
@@ -1,8 +1,26 @@
+var keys = Object.keys(0);
 
-
-const keys = Object.keys(0);
 let values = [];
-for (let i = 0; i < keys.length; i++){
-    let key = keys[i];
-    values[i] = 0[keys];
+
+function test() {
+    for (let i = 0; i < keys.length; i++){
+        let key = keys[i];
+        values[i] = 0[key];
+    }
+
 }
+
+function test2() {
+    for (var i = 0; i < a.length; i++) {
+        if (!a[i]) continue; // P // corpo do laço aqui
+
+    }
+}
+
+function defaultFunction() {
+    for(var i = 0, len = keys.length; i < len; i++) {
+        // o corpo do laço permanece o mesmo
+    }
+}
+
+console.log(test2())
