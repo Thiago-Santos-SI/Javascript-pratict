4bed72dfacd992afada2a17257fbba31895300e4
Thiago-Santos-SI <thiiagosantos300@gmail.com>
2021-08-24 04:29:28 -0300

    addminutes

---
+++ addMinutes.js
@@ -0,0 +1,10 @@
+var addMinutes = require('date-fns/addMinutes')
+
+const date = new Date();
+
+console.log(date)
+
+const result = addMinutes(date, 30)
+
+console.log(result)
+
--- teste.js
+++ teste.js
@@ -2,8 +2,10 @@ string = "HashMap<List<Integer>, HashMap<String, List<List<Double>>>>, test: str
 
 
 for (let i = 0; i < string.length; i++) {
-    console.log(string[i] + "- " + i)
     if (i === 5){
         i = 10
     }
 }
+
+
+console.log('a              ,            t'.trim())
